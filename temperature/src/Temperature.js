8231348de91211991b3c41548777689a01c5df4b
Gurjantsingh1 <W0782841@myscc.ca>
2022-10-16 22:49:47 -0400

    done

--- temperature/src/Temperature.js
+++ temperature/src/Temperature.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 const Temperature = () => {
 	const [temperatureValue, setTemperatureValue] = useState(10);
-	
+	const [temperatureColor, setTemperatureColor] = useState('cold');
     const [temp, setTemp] = useState("C")
 	function convertTemperature(){
 		if (temp === "F"){
@@ -18,18 +18,22 @@ const Temperature = () => {
 		const newTemperature = temperatureValue + 1;
 		setTemperatureValue(newTemperature);
 
-		
+		if (newTemperature >= 16) {
+			setTemperatureColor('hot');
+		}
 	};
 	const decreaseTemperature = () => {
 		const newTemperature = temperatureValue - 1;
 		setTemperatureValue(newTemperature);
-		
+		if (newTemperature < 16) {
+			setTemperatureColor('cold');
+		}
 	
 	};
 	return (
 		<div className='temp-container'>
 			<div className='temperature-display-container'>
-				<div className={`temperature-display`}>{temperatureValue}</div>
+				<div className={`temperature-displa ${temperatureColor}`}>{temperatureValue}</div>
 			</div>
 			<div className='button-container'>
                 <button onClick={decreaseTemperature}>-</button>
